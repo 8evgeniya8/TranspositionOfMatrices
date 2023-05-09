aee96df547d1fc8663500a4a1fef1d4708022a75
Tsukanova Evgeniya <tata@gmail.com>
2023-05-09 12:04:58 +0300

    1 commit

--- src/Tsukanova/Transposition.java
+++ src/Tsukanova/Transposition.java
@@ -6,52 +6,73 @@ import java.util.Scanner;
 //        Після введення повинні автоматично створюватися два масиви
 //        розміром M x N перший та N x M другий.
 public class Transposition {
-    public static void main(String[] args) {
+    public static void main(String[]args) {
         Scanner sca = new Scanner(System.in);
         System.out.println("Ввести розмір матриці");
         System.out.println("Кількість рядків");
-        int userValue1 = 0;
-        int userValue2 = 0;
+        int numRows;
+        int numColums;
         while (true) {
-            if (sca.hasNextInt()) {
-                userValue1 = sca.nextInt();
-                sca.nextLine();
-                break;
+            if (sca.hasNextInt()){
+                numRows = sca.nextInt();
+                if (numRows > 0) {
+                    break;
+                }
+               else  {
+                    System.out.println("Введи число від 0");
+                    continue;
+                }
+
             } else {
-                System.out.println("Помилка. Введіть лише число");
-                sca.nextLine();
+                System.out.println("Помилка.Введи число");
             }
+            sca.nextLine();
         }
         System.out.println("Тепер кількість стовпців в матриці");
         while (true) {
-            if (sca.hasNextInt()) {
-                userValue2 = sca.nextInt();
-                sca.nextLine();
-                break;
+            if (sca.hasNextInt()){
+                numColums = sca.nextInt();
+                if (numColums > 0) {
+                    break;
+                }
+                else  {
+                    System.out.println("Введи число від 0");
+                    continue;
+                }
+
             } else {
-                System.out.println("Помилка. Введіть лише число");
-                sca.nextLine();
+                System.out.println("Помилка.Введи число");
+            }
+            sca.nextLine();
+        }
+
+        int[][] matrix = new int[numRows][numColums];
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numColums; j++) {
+                matrix[i][j] = (int) (Math.random() * 10);
             }
         }
-        int n = userValue1;
-        int m = userValue2;
-        int[][] array = new int[m][n];
-        int[][] array2 = new int[n][m];
+        int[][] Matrix2 = new int[numColums][numRows];
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numColums; j++) {
+                Matrix2[j][i] = matrix[i][j];
+            }
+        }
 
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                array[i][j] = (int) (Math.random() * 10);
-                System.out.print(array[i][j] + "\t");
+        // Print the original matrix
+        System.out.println("Original Matrix:");
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numColums; j++) {
+                System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
 
-        System.out.println("\n");
-
-        for (int i = 0; i < array2.length; i++) {
-            for (int j = 0; j < array2[i].length; j++) {
-                array2[i][j] = (int) (Math.random() * 10);
-                System.out.print(array2[i][j] + "\t");
+        // Print the transposed matrix
+        System.out.println("Transposed Matrix:");
+        for (int i = 0; i < numColums; i++) {
+            for (int j = 0; j < numRows; j++) {
+                System.out.print(Matrix2[i][j] + " ");
             }
             System.out.println();
         }
